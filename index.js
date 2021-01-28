375a6db130e4310f27732eea2d43cbe6cd90fc51
Marta <marta.gilabert.guzman@gmail.com>
2021-01-28 15:32:35 +0100

    local storage

--- index.js
+++ index.js
@@ -239,16 +239,19 @@ playbtn.addEventListener('click', ()=>{
 
 //LOCAL STORAGE
 
+
+
 let itemsArray = localStorage.getItem('items')
   ? JSON.parse(localStorage.getItem('items'))
   : []
 
+
 localStorage.setItem('items', JSON.stringify(itemsArray))
-const data = JSON.parse(localStorage.getItem('items'))
+
 
 const liMaker = (text) => {
   const li = document.createElement('li')
-  li.textContent = `Gamer : ${text} , Score : ${score}`
+  li.textContent = `Gamer : ${text}, Score : ${score}`
   ul.appendChild(li)
 }
 
@@ -259,10 +262,6 @@ form.addEventListener('submit', function (e) {
   localStorage.setItem('items', JSON.stringify(itemsArray))
   liMaker(input.value)
   input.value = ''
-})
-
-data.forEach((item) => {
-  liMaker(item)
 })
 
 clear.addEventListener('click', function () {
