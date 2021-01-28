b1f47bfa9ad3d8582dff80ef2c12820fb1402ecc
Marta <marta.gilabert.guzman@gmail.com>
2021-01-28 15:03:28 +0100

    second try local storage

--- index.js
+++ index.js
@@ -239,35 +239,35 @@ playbtn.addEventListener('click', ()=>{
 
 //LOCAL STORAGE
 
-// let itemsArray = localStorage.getItem('items')
-//   ? JSON.parse(localStorage.getItem('items'))
-//   : []
+let itemsArray = localStorage.getItem('items')
+  ? JSON.parse(localStorage.getItem('items'))
+  : []
 
-// localStorage.setItem('items', JSON.stringify(itemsArray))
-// const data = JSON.parse(localStorage.getItem('items'))
+localStorage.setItem('items', JSON.stringify(itemsArray))
+const data = JSON.parse(localStorage.getItem('items'))
 
-// const liMaker = (text) => {
-//   const li = document.createElement('li')
-//   li.textContent = `${text}  Score : ${score}`
-//   ul.appendChild(li)
-// }
+const liMaker = (text) => {
+  const li = document.createElement('li')
+  li.textContent = `Gamer : ${text} , Score : ${score}`
+  ul.appendChild(li)
+}
 
-// form.addEventListener('submit', function (e) {
-//   e.preventDefault()
+form.addEventListener('submit', function (e) {
+  e.preventDefault()
 
-//   itemsArray.push(input.value)
-//   localStorage.setItem('items', JSON.stringify(itemsArray))
-//   liMaker(input.value)
-//   input.value = ''
-// })
+  itemsArray.push(input.value)
+  localStorage.setItem('items', JSON.stringify(itemsArray))
+  liMaker(input.value)
+  input.value = ''
+})
 
-// data.forEach((item) => {
-//   liMaker(item)
-// })
+data.forEach((item) => {
+  liMaker(item)
+})
 
-// clear.addEventListener('click', function () {
-//     localStorage.clear()
-//     while (ul.firstChild) {
-//       ul.removeChild(ul.firstChild)
-//     }
-//   })
+clear.addEventListener('click', function () {
+    localStorage.clear()
+    while (ul.firstChild) {
+      ul.removeChild(ul.firstChild)
+    }
+  })
